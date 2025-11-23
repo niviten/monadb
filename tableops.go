f21adbdec744e3ad5e5ba995390cfdde825a1972
niviten <l.j.nivethan@gmail.com>
2025-11-23 19:49:09 +0530

    Implement insert row in table

--- tableops.go
+++ tableops.go
@@ -3,7 +3,9 @@ package monadb
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 
+	"github.com/niviten/monadb/internal/fileops"
 	"github.com/niviten/monadb/internal/sqlite"
 )
 
@@ -15,22 +17,28 @@ type mcolumn struct {
 
 func Insert(tableName string, data map[string]any) (uint64, error) {
 	conn := sqlite.GetConn()
-	rows, err := conn.Query(`
+	query := `
 		SELECT c.name, c.size, c.data_type_id
 		FROM mcolumn c
 			LEFT JOIN mtable t
 				ON c.table_id = t.id
-		WHERE t.name = "?"`, tableName)
+		WHERE t.name = ?`
+	rows, err := conn.Query(query, tableName)
 	if err != nil {
 		log.Printf("Error while querying columns for table name %s: %s\n", tableName, err.Error())
 		return 0, fmt.Errorf("internal server error")
 	}
+	defer rows.Close()
 	columnList := make([]*mcolumn, 0)
 	for rows.Next() {
 		mc := &mcolumn{}
 		err = rows.Scan(&mc.name, &mc.size, &mc.dtid)
 		if err != nil {
 			log.Printf("Error while scanning table column for table name %s: %s\n", tableName, err.Error())
+			return 0, fmt.Errorf("internal server error")
+		}
+		if mc.size == -1 {
+			mc.size = sizeOfDataType(DataType(mc.dtid))
 		}
 		columnList = append(columnList, mc)
 	}
@@ -39,7 +47,8 @@ func Insert(tableName string, data map[string]any) (uint64, error) {
 	for _, col := range columnList {
 		val := data[col.name]
 		dataBytes, err := valToBytes(DataType(col.dtid), val, col.size)
-		if err == InvalidDataType {
+		if err == ErrInvalidDataType {
+			log.Printf("InvalidDataType error for %s :: datatype: %d :: val: %v\n", col.name, col.dtid, val)
 			return 0, err
 		}
 		if err != nil {
@@ -47,9 +56,16 @@ func Insert(tableName string, data map[string]any) (uint64, error) {
 			return 0, fmt.Errorf("internal server error")
 		}
 		blist = append(blist, dataBytes)
-		totalSize = totalSize + col.size
+		totalSize = totalSize + len(dataBytes)
 	}
 	rowBytes := mergeBytes(totalSize, blist)
+
+	tablePath := filepath.Join(dataDirPath, "default", tableName, "table_data")
+	if err = fileops.Append(tablePath, rowBytes); err != nil {
+		log.Printf("Error while inserting rowbytes to file path %s, error: %s\n", tablePath, err.Error())
+		return 0, fmt.Errorf("internal server error")
+	}
+
 	return 0, nil
 }
 
